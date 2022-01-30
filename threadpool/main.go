41fc145474b6f22c47e8a3445120f1ec05320ba4
Felipe Vieira <felipe.vieira@nava.com.br>
2022-01-30 10:37:20 -0300

    Add thread pool to polygon area calc

--- threadpool/main.go
+++ threadpool/main.go
@@ -8,6 +8,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -16,34 +17,47 @@ type Point2D struct {
 	y int
 }
 
+const numberOfThreads int = 16
+
 var r = regexp.MustCompile(`\((\d*),(\d*)\)`)
+var waitGroup sync.WaitGroup
 
 func main() {
-
 	absPath, _ := filepath.Abs("./threadpool/")
 	dat, _ := ioutil.ReadFile(filepath.Join(absPath, "polygons.txt"))
 	text := string(dat)
 
+	inputChannel := make(chan string, 1000)
+	for i := 0; i < numberOfThreads; i++ {
+		go findArea(inputChannel)
+	}
+
 	start := time.Now()
 	for _, line := range strings.Split(text, "\n") {
-		findArea(line)
+		waitGroup.Add(1)
+		inputChannel <- line
 	}
+	close(inputChannel)
+	waitGroup.Wait()
 	elapsed := time.Since(start)
 	fmt.Printf("elapsed : %s \n", elapsed)
 }
 
-func findArea(pointStr string) {
-	var points []Point2D
-	for _, p := range r.FindAllStringSubmatch(pointStr, -1) {
-		x, _ := strconv.Atoi(p[1])
-		y, _ := strconv.Atoi(p[2])
-		points = append(points, Point2D{x, y})
-	}
+func findArea(inputChannel chan string) {
+	for pointStr := range inputChannel {
+		var points []Point2D
+		for _, p := range r.FindAllStringSubmatch(pointStr, -1) {
+			x, _ := strconv.Atoi(p[1])
+			y, _ := strconv.Atoi(p[2])
+			points = append(points, Point2D{x, y})
+		}
 
-	area := 0.0
-	for i := 0; i < len(points); i++ {
-		a, b := points[i], points[(i+1)%len(points)]
-		area += float64(a.x*b.y) - float64(a.y*b.x)
+		area := 0.0
+		for i := 0; i < len(points); i++ {
+			a, b := points[i], points[(i+1)%len(points)]
+			area += float64(a.x*b.y) - float64(a.y*b.x)
+		}
+		fmt.Println(math.Abs(area) / 2.0)
+		waitGroup.Done()
 	}
-	fmt.Println(math.Abs(area) / 2.0)
 }
